bcd410d26a07b4d1ee2f2df4b0904c9d54e77b02
InnaXITE <inna@xite.com>
2018-05-09 13:20:55 +0200

    X-Timig header added as a middleware component

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative 'boot'
+require File.expand_path('../boot',__FILE__)
 
 require 'rails/all'
+
+require_relative '../lib/timer'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +11,8 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
+    config.middleware.insert_before(Rack::Sendfile, Timer)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
